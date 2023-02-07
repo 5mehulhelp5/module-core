758c5043e55b602e416b190a11464da1a7823f08
alexsolonenko <oleksii.solonenko@leanswift.com>
2023-02-07 12:16:43 +0200

    Add composer.json

--- Helper/Data.php
+++ Helper/Data.php
@@ -2,19 +2,15 @@
 
 namespace Commerce365\Core\Helper;
 
-use GuzzleHttp\Client;
+use Commerce365\Core\Logger\Logger;
 use Magento\Framework\App\Helper\AbstractHelper;
 
 class Data extends AbstractHelper
 {
-    protected $_hubUrl;
     protected $_logger;
 
-    public function __construct(
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Commerce365\Core\Logger\Logger $logger
-    ) {
-        $this->_hubUrl = $scopeConfig->getValue('commerce365config_general/hub/hub_url', 'website');
+    public function __construct(Logger $logger)
+    {
         $this->_logger = $logger;
     }
 
