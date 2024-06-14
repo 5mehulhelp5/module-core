59b83b328038fb2d939962d6221045b8b2da3415
alexsolonenko <oleksii.solonenko@leanswift.com>
2024-06-15 00:04:36 +0300

    CORE-143 - make fields in advanced config required

--- Service/Request/BusinessCentral/GetBCEndpointUrl.php
+++ Service/Request/BusinessCentral/GetBCEndpointUrl.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Commerce365\Core\Service\Request\BusinessCentral;
 
 use Commerce365\Core\Model\AdvancedConfig;
+use Magento\Framework\Exception\LocalizedException;
 
 class GetBCEndpointUrl
 {
@@ -24,11 +25,11 @@ class GetBCEndpointUrl
         if ($this->advancedConfig->isBCOAuth()) {
             $tenantId = $this->advancedConfig->getTenantId();
             if (!$tenantId) {
-                return '';
+                throw new LocalizedException(__('Tenant Id should be configured'));
             }
             $endpoint = rtrim($endpoint, '/') . '/' . $tenantId;
         }
-            
+
         return rtrim($endpoint, '/') . '/' . $environment . '/ODataV4/' . $method . '?company=' . rawurlencode($company);
     }
 }
